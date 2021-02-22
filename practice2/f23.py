482a927aa82ffbf8279955dfa4c10eed7efd3637
aleksey136 <leha.lebedev.02@list.ru>
2021-02-22 14:08:31 +0300

    Added

--- practice2/f23.py
+++ practice2/f23.py
@@ -1,4 +1,4 @@
-def function(x):
+def f21(x):
     output = -1
     if x[0] == "tea":
         if x[4] == "2010" or x[4] == 2010:
@@ -30,8 +30,34 @@ def function(x):
     return output
 
 
-if __name__ == '__main__':
-    xUser = [str, str, str, str, str]
-    for i in range(5):
-        xUser[i] = input()
-    print(str(function(xUser)))
+def f22(line):
+    line2 = str(bin(int(line, 16)))
+    while len(line2) != 34:
+        line2 = "0b0" + line2[2:]
+    A = line2[22:]
+    B = line2[10:22]
+    C = line2[4:10]
+    D = line2[3]
+    E = line2[2]
+    line2 = "0b" + D + A + B + E + C
+    return hex(int(line2, 2))
+
+
+def f23(matrix):
+    matrix2 = []
+    for i in range(len(matrix)):
+        flag = 0
+        for j in range(len(matrix[i])):
+            if matrix[i][j] is None:
+                flag = flag + 1
+        if flag != 3:
+            matrix2.append(matrix[i])
+    matrix = []
+    for i in range(len(matrix2)):
+        a = "{6}{7}{8}{9}/{3}{4}/{0}{1}".format(*matrix2[i][0].split(";")[0])
+        b = matrix2[i][0].split(";")[1]
+        c = b.split(" ")[0]
+        matrix.append([c, str(round(float(matrix2[i][1]), 1)), a,
+                       "{5}{6}{7}-{8}{9}-{10}{11}".format(*matrix2[i][2])])
+    matrix.sort(key=lambda x: x[3])
+    return matrix
