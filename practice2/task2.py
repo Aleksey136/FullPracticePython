c4e70bc7c0959643a6a0d02f0a201d5d7b1496cc
Aleksey136 <leha.lebedev.02@list.ru>
2021-02-26 16:55:26 +0300

    Added

--- practice2/task2.py
+++ practice2/task2.py
@@ -1,16 +1,12 @@
-def function(line):
-    line2 = str(bin(int(str(line), 16)))
-    while len(line2) != 34:
-        line2 = "0b0" + line2[2:]
-    A = line2[22:]
-    B = line2[10:22]
-    C = line2[4:10]
-    D = line2[3]
-    E = line2[2]
-    line2 = "0b" + D + A + B + E + C
-    return str(hex(int(line2, 2)))
+def function(line: hex):
+    line2 = ((line >> 30) & 0x1) << 31
+    line2 = line2 | ((line & 0x00000fff) << 19)
+    line2 = line2 | (((line >> 12) & 0xfff) << 7)
+    line2 = line2 | (((line >> 31) & 0x1) << 6)
+    line2 = line2 | ((line >> 24) & 0x3f)
+    return hex(line2)
 
 
 if __name__ == '__main__':
-    inputUser = input()
-    print(function(inputUser))
+    x = 0x8f6e6557
+    print(function(x))
